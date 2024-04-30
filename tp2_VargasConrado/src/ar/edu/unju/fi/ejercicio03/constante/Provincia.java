b2dcd69b6eef9a838af9d3ad8ee0f685a4d9be6b
Conrado Vargas <conradovargas93@gmail.com>
2024-04-30 17:37:50 -0300

    Ejercicio 3 finalizado

--- tp2_VargasConrado/src/ar/edu/unju/fi/ejercicio03/constante/Provincia.java
+++ tp2_VargasConrado/src/ar/edu/unju/fi/ejercicio03/constante/Provincia.java
@@ -1,5 +1,26 @@
 package ar.edu.unju.fi.ejercicio03.constante;
 
 public enum Provincia {
+	JUJUY(500000, 53200), SALTA(1200000, 155488), TUCUMAN(1700000, 222524), CATAMARCA(400000, 102606),
+	LARIOJA(350000, 89680), SANTIAGO_DEL_ESTERO(900000, 136351);
 
+	private final int cantidadPoblacion;
+	private final int superficie;
+
+	private Provincia(int cantidadPoblacion, int superficie) {
+		this.cantidadPoblacion = cantidadPoblacion;
+		this.superficie = superficie;
+	}
+
+	public int getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+
+	public int getSuperficie() {
+		return superficie;
+	}
+
+	public double calcularDensidadPoblacional() {
+		return (double) cantidadPoblacion / superficie;
+	}
 }
--- tp2_VargasConrado/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ tp2_VargasConrado/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,10 +1,18 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import ar.edu.unju.fi.ejercicio03.constante.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		Provincia[] provincias = Provincia.values();
 
+		for (Provincia provincia : provincias) {
+			System.out.println("Provincia: " + provincia);
+			System.out.println("Cantidad de población: " + provincia.getCantidadPoblacion());
+			System.out.println("Superficie: " + provincia.getSuperficie());
+			System.out.println("Densidad poblacional: " + provincia.calcularDensidadPoblacional());
+			System.out.println();
+		}
 	}
-
 }
